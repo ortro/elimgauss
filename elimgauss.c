4e64584852847e9d9ee0fc55e4cc6a3a039fe916
Pedro Henrique De Lira Alves <pedrohlaaec@gmail.com>
2018-05-21 16:23:32 -0300

    adicionado txt e organizado estetica

--- elimgauss.c
+++ elimgauss.c
@@ -2,19 +2,17 @@
 
 #define LIN 4
 #define COL 5
-#define NITMAX 5
-
-void printmat(float matriz[][COL]);
+#define NITMAX 11
 
 int main(void)
 {
     /* Declaracao de variaveis decisorias*/
     float matriz[LIN][COL] =
     {
-        {2, 2, 1, 1, 7},
-        {1, -1, 2, -1, 1},
-        {3, 2, -3, -2, 4},
-        {4, 3, 2, 1, 12},
+        {1.234, 5.331, 1.231, -2.174, 6.456},
+        {1.333, 2.346, -4.234, 3.147, 2.502},
+        {2.204, -2.021, 6.001, -1.125, -1.101},
+        {0.211, 7.126, -0.234, 5.003, -2.115},
     };
     /*indices*/
     int i,j,k,w,x,y,z;
@@ -24,16 +22,22 @@ int main(void)
     float mult[LIN];
     float matriz00[LIN][COL];
     float s[NITMAX+1][LIN];
-    
+
 
     //Solucoes
-    s[0][0] = 1;
-    s[0][1] = 0;
-    s[0][2] = 0;
-    s[0][3] = 1;
+    s[0][0] = 0.02;
+    s[0][1] = 0.25;
+    s[0][2] = 0.75;
+    s[0][3] = 0.12;
 
-    printf("===Matriz dada=== \n");
-    printmat(matriz);
+    printf("===Matriz dada===\n\n");
+    for(i=0;i<LIN;i++)
+    {
+        for(j=0;j<COL; j++)
+            printf("%0.4f\t", matriz[i][j]);
+        printf("\n");
+    }
+
     printf("\n\n\n");
 
     for(i=0;i<LIN; i++)
@@ -73,16 +77,21 @@ int main(void)
                 matriz[x][y]= matriz00[x][y];
             }
         }
+
+
+        printf("\n===MATRIZ EXPANDIDA===\n");
         for(w=0;w<LIN;w++)
         {
+            printf("\n");
             for(z=0;z<COL;z++)
             {
                 if(w<=i+1)
-                    printf("\t%0.4f(%d)\t", matriz[w][z], w);
+                    printf("%0.4f(%d)\t", matriz[w][z], w);
                 else
-                    printf("\t%0.4f(%d)\t", matriz[w][z], i+1);
+                    printf("%0.4f(%d)\t", matriz[w][z], i+1);
             }
             printf("\n");
+
         }
 
         printf("\n\n\n\n");
@@ -106,21 +115,13 @@ int main(void)
 
     for(k=0; k < NITMAX; k++)
     {
+        printf("\nSolucao %d:",k);
         printf("\n");
         for(i=0; i < LIN; i++)
-            printf("%f\t", s[k][i]);
-    }
-
-   return 0;
-}
-void printmat(float matriz[][COL])
-{
-    int i, j;
-    for(i=0;i<LIN;i++)
-    {
-        for(j=0;j<COL; j++)
-            printf("\t%0.4f\t", matriz[i][j]);
+            printf("%0.4f\t", s[k][i]);
         printf("\n");
     }
+
+    return 0;
 }
 
